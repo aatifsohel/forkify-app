52a62acb41f0c03b40fe8ee98b688685af933b44
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2023-06-25 14:09:03 +0530

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3f9b1832-3756-406f-b3ff-ff080f3cd427';
+export const MODAL_CLOSE_SEC = 2.5;
