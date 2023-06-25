e2b6f3b73428fb9338ca9a4cc6e780bb208a7766
Aatif Sohel <84847205+aatifsohel@users.noreply.github.com>
2023-06-25 14:35:30 +0530

    clean some comments

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,10 +11,6 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import paginationView from './views/paginationView.js';
 
-// if (module.hot) {
-//   module.hot.accept();
-// }
-
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
